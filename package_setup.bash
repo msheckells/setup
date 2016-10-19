cb5c80f6d4cff07cc73b5676f82a59d22f70d8c6
Matthew Sheckells <msheckells@gmail.com>
2016-10-19 17:23:53 -0400

    Add more ros packages

--- package_setup.bash
+++ package_setup.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 APT_PACKAGES="vim git cmake-gui build-essential python-catkin-tools ros-indigo-desktop 
-              nvidia-cg-toolkit libois-dev libois-1.3.0 libeigen3-dev freeglut3-dev"
+              nvidia-cg-toolkit libois-dev libois-1.3.0 libeigen3-dev freeglut3-dev
+              python2.7 python-pip libav-tools python-scipy libblas-dev liblapack-dev 
+              libatlas-base-dev gfortran ros-indigo-enu ros-indigo-gazebo-msgs
+              ros-indigo-camera-info-manager libusb-1.0-0-dev ros-indigo-pcl-conversions
+              ros-indigo-pcl-ros ros-indigo-stereo-image-proc ros-indigo-gps-common
+              ros-indigo-ar-track-alvar-msgs ros-indigo-map-server ros-indigo-image-view"
 
 #if [[ $EUID -ne 0 ]]; then
 #   echo "This script must be run as root" 1>&2
@@ -55,6 +60,57 @@ cmake ..
 make -j4
 sudo make install -j4
 
+#pcl
+cd ~/src
+git clone https://github.com/PointCloudLibrary/pcl.git
+cd pcl
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install -j5
+
+#sophus
+cd ~/src
+git clone https://github.com/stonier/sophus.git
+cd sophus
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install -j5
+sudo cp /usr/local/share/sophus/sophusConfig.cmake /usr/share/cmake-2.8/Modules/.
+
+#toon
+cd ~/src
+wget https://www.edwardrosten.com/cvd/TooN-2.2.tar.gz
+tar -zxvf TooN-2.2.tar.gz
+rm TooN-2.2.tar.gz
+cd TooN-2.2
+./configure
+sudo make install
+
+#object_renderer
+cd ~/src
+git clone https://github.com/msheckells/object_renderer.git
+cd object_renderer
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
+
+# ceres
+cd ~/src
+git clone https://ceres-solver.googlesource.com/ceres-solver
+cd ceres-solver
+git checkout 1.10.0rc2
+mkdir build
+cd build
+cmake .. -DBUILD_SHARED_LIBS
+make -j4
+sudo make install
+
 # gcop
 cd ~/src
 git clone git@git.lcsr.jhu.edu:mkobila1/gcop.git
@@ -64,4 +120,3 @@ cd build
 cmake ..
 make -j4
 sudo make install -j4
-
